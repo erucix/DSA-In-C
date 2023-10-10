730c2f34b7324451d4c173f851ff11d2c35bfdd3
erucix <erucix@gmail.com>
2023-10-11 03:42:22 +0530

    modified:   Lecture 4/poly_operations_linked.c 	new file:   Lecture 4/sparse.c

--- Lecture 4/poly_operations_linked.c
+++ Lecture 4/poly_operations_linked.c
@@ -72,6 +72,7 @@ int main()
 
     Poly *head1 = NULL;
     Poly *head2 = NULL;
+    Poly *head3 = NULL;
 
     for (int i = 0, j = size1; i < size1; i++, j--)
     {
@@ -80,6 +81,21 @@ int main()
     for (int i = 0, j = size2; i < size2; i++, j--)
     {
         insertPoly(&head2, i, j, i);
+    }
+
+    Poly *temp1 = head1;
+    Poly *temp2 = head2;
+    Poly *temp3 = head3;
+
+    for (int i = 0; i < length(&head1); i++)
+    {
+        for (int j = 0; j < length(&head2); j++)
+        {
+            temp3->coff += temp1->coff * temp2->coff;
+            temp3->pwr += temp1->pwr + temp2->pwr;
+            temp2 = temp2->next;
+        }
+        temp1 = temp1->next;
     }
 
     traverse(&head1);
