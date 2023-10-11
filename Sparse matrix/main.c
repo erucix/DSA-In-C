a28cdd8d42660517abea915e2a5b73312219dd97
erucix <erucix@gmail.com>
2023-10-11 20:02:27 +0530

    modified:   Sparse matrix/main.c

--- Sparse matrix/main.c
+++ Sparse matrix/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+void usingArray()
 {
     int sparse[3][2] = {{2, 0}, {0, 5}, {0, 0}};
     int nonZero = 0;
@@ -31,15 +31,97 @@ int main(int argc, char **argv)
                 newSparse[k][0] = i;
                 newSparse[k][1] = j;
                 newSparse[k][2] = sparse[i][j];
+                k++;
+            }
+        }
+    }
+    printf("R | C | N\n");
+    printf("---------\n");
+    for (int i = 0; i <= nonZero; i++)
+    {
+        printf("%d   %d   %d\n", newSparse[i][0], newSparse[i][1], newSparse[i][2]);
+    }
+}
+
+typedef struct Node
+{
+    int x;
+    int y;
+    int z;
+    struct Node *next;
+} Node;
+
+void createNode(Node **head, int x, int y, int z)
+{
+    Node *newNode = malloc(sizeof(Node));
+    newNode->x = x;
+    newNode->y = y;
+    newNode->z = z;
+
+    Node *temp = *head;
+
+    if (temp != NULL)
+    {
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        newNode->next = NULL;
+        temp->next = newNode;
+    }
+    else
+    {
+        *head = newNode;
+        newNode->next = NULL;
+    }
+}
+
+void usingLinkedList()
+{
+    Node *head = NULL;
+
+    int sparse[3][2] = {{2, 0}, {0, 5}, {0, 0}};
+    int nonZero = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            if (sparse[i][j] != 0)
+            {
+                nonZero++;
             }
         }
     }
 
-    for (int i = 0; i < nonZero; i++)
+    createNode(&head, 3, 2, nonZero);
+
+    for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < 2; j++)
         {
-            printf("%d   %d   %d\n", newSparse[i][0], newSparse[i][1], newSparse[i][2]);
+            if (sparse[i][j] != 0)
+            {
+                createNode(&head, i, j, sparse[i][j]);
+            }
         }
     }
+
+    Node *temp = head;
+
+    printf("\n\nR | C | N\n");
+    printf("---------\n");
+
+    while (temp != NULL)
+    {
+        printf("%d   %d   %d\n", temp->x, temp->y, temp->z);
+        temp = temp->next;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    usingArray();
+    usingLinkedList();
 }
