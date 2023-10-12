f6c9005fa1fb32b75898433fdb28fb277c5cbab4
erucix <erucix@gmail.com>
2023-10-12 19:50:43 +0530

    modified:   Linked List/middle_node.c

--- Linked List/middle_node.c
+++ Linked List/middle_node.c
@@ -69,8 +69,10 @@ void randomize(Node **head, int limit)
 {
     for (int i = 0; i < limit; i++)
     {
-        insert(head, i, i);
+        insert(head, i, rand() % 10);
     }
+
+    traverse(head);
 }
 
 void middle(Node **head)
@@ -127,9 +129,131 @@ void rotate(Node **head, int step)
 {
     Node *temp = *head;
 
-    for (int i = 0; i < step && temp != NULL; i++)
+    int k = 1;
+
+    while (k < step && temp != NULL)
     {
         temp = temp->next;
+        k++;
+    }
+
+    Node *kthNode = temp;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = *head;
+    *head = kthNode->next;
+    kthNode->next = NULL;
+}
+
+void nthFromEnd(Node **head, int pos)
+{
+    Node *temp = *head;
+    int len = length(head);
+
+    for (int i = 0; i < len - pos; i++)
+    {
+        temp = temp->next;
+    }
+
+    printf("\n%dth data is %d\n", pos, temp->data);
+}
+
+void deleteLast(Node **head)
+{
+    Node *temp = *head;
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+    Node *freeTemp = temp->next;
+    temp->next = NULL;
+    free(freeTemp);
+}
+
+void deleteMiddle(Node **head)
+{
+    Node *slow = *head;
+    Node *fast = *head;
+    Node *prev = NULL;
+
+    while (slow != NULL && fast != NULL)
+    {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    prev->next = slow->next;
+
+    free(slow);
+}
+
+void deleteGivenNodes(Node **head, int pos, int num)
+{
+    Node *temp = *head;
+
+    int counter = 0;
+
+    while (temp != NULL && counter < pos)
+    {
+        temp = temp->next;
+        counter++;
+    }
+
+    Node *nthNode = temp;
+    Node *prev = NULL;
+    int counter1 = 0;
+
+    while (temp != NULL && counter1 <= num)
+    {
+        prev = temp;
+        temp = temp->next;
+        counter1++;
+    }
+
+    nthNode->next = temp->next;
+}
+
+void sort(Node **head)
+{
+    Node *p1 = *head;
+
+    for (int i = 0; i <= length(head); i++)
+    {
+        p1 = *head;
+        for (int j = 0; j < length(head) - i - 1; j++)
+        {
+            if (p1->data > p1->next->data)
+            {
+                int temp = p1->data;
+                p1->data = p1->next->data;
+                p1->next->data = temp;
+            }
+            p1 = p1->next;
+        }
+    }
+}
+
+void deleteDuplicates(Node **head)
+{
+    Node *temp = *head;
+    Node *next = NULL;
+
+    while (temp->next != NULL)
+    {
+        if (temp->data == temp->next->data)
+        {
+            next = temp->next->next;
+            free(temp->next);
+            temp->next = next;
+        }
+        else
+        {
+            temp = temp->next;
+        }
     }
 }
 
@@ -138,10 +262,10 @@ int main(int argc, char **argv)
     Node *head = NULL;
 
     randomize(&head, 10);
-
+    sort(&head);
     traverse(&head);
 
-    reverse(&head);
+    deleteDuplicates(&head);
 
     traverse(&head);
 }
